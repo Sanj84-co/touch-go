b19fe7f842a2370f9901c27b830b74b157705f65
Sanj84-co <Sansenus123@gmail.com>
2025-02-01 18:49:08 -0500

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
-import { Text, StyleSheet, SafeAreaView, Modal, View, Pressable } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, Modal, View, Pressable, TextInput } from 'react-native';
 import React, { useState } from 'react';
+import Slider from '@react-native-community/slider';
 
 export default function HomeScreen() {
 
@@ -11,35 +12,57 @@ export default function HomeScreen() {
 
   const [moveVis, setMoveVis] = useState(false);
 
+  const [text, setText] = useState('');
+
+  const [bgCol, setBgCol] = usestate("red");
+
+  const [switchCol, setSwitchCol] = useState("black");
+  
+  let colArr = ['white', 'green', 'yellow'];
+
   return (
     <SafeAreaView style={styles.mainView}>
       <Modal visible={clickVis} animationType="fade" onRequestClose={() => setClickVis(false)} presentationStyle="pageSheet">
         <View style={styles.container}>
           <Text style={styles.largeText}>Click!</Text>
+          <Pressable onPress={() => setBgCol(colArr[Math.floor(Math.random() * 3)]}>
+            <Text style={{ margin: 'auto', marginBottom: 50, borderRadius: 90, padding: 75, backgroundColor: bgCol, }}> </Text>
+          </Pressable>
         </View>
       </Modal>
       <Modal visible={swipeVis} animationType="fade" onRequestClose={() => setSwipeVis(false)} presentationStyle="pageSheet">
-        <View style={styles.container}>
-          <Text style={styles.largeText}>Swipe!</Text>
+        <View style={{ backgroundColor: switchCol, styles.container }}>
+          <Text style={styles.largeText}>Switch</Text>
+          <Pressable onPress={() => setSwitchCol('white')}>
+          <Text style={{ padding: 40, backgroundColor: 'white', margin: 'auto', }}> </Text>
+          </Pressable>
         </View>
       </Modal>
       <Modal visible={typeVis} animationType="fade" onRequestClose={() => setTypeVis(false)} presentationStyle="pageSheet">
         <View style={styles.container}>
           <Text style={styles.largeText}>Type!</Text>
+          <TextInput 
+            style = {styles.input}
+            placeholder = "Start Typing"
+            onChangeText = {setText}
+            value = {text}
+            ></TextInput>
         </View>
       </Modal>
       <Modal visible={moveVis} animationType="fade" onRequestClose={() => setMoveVis(false)} presentationStyle="pageSheet">
         <View style={styles.container}>
-          <Text style={styles.largeText}>Move</Text>
+          <Text style={styles.largeText}>Swipe!</Text>
+          <Slider style={{ width: 200, height: 40, }} minimumValue={0} maximumValue={1} minimumTrackTintColor="#FFFFFF" maximumTrackTintColor="#000000" />
         </View>
       </Modal>
         <Text style={styles.largeText}>Welcome to Touch and Go</Text>
         <Pressable onPress={() => setClickVis(true)} style={styles.buttons}>Click</Pressable>
-        <Pressable onPress={() => setSwipeVis(true)} style={styles.buttons}>Swipe</Pressable>
+        <Pressable onPress={() => setSwipeVis(true)} style={styles.buttons}>Switch</Pressable>
         <Pressable onPress={() => setTypeVis(true)} style={styles.buttons}>Type</Pressable>
-        <Pressable onPress={() => setMoveVis(true)} style={styles.buttons}>Move</Pressable>
+        <Pressable onPress={() => setMoveVis(true)} style={styles.buttons}>Swipe</Pressable>
     </SafeAreaView>
   );
+  
 }
 
 const styles = StyleSheet.create({
@@ -78,5 +101,9 @@ const styles = StyleSheet.create({
     shadowOffset: {width: 0, height: 1},
     shadowOpacity: 1,
     shadowRadius: 3,
+  },
+  input: {
+    padding: 10,
+    borderRadius: 15,
   },
 });
