0f62e49d79a135d197d11e7e7d549a7b3eb27918
Will OKeefe <tidalwithstyle@gmail.com>
2025-02-01 11:55:54 -0500

    Set up Home Page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,13 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { StyleSheet, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
-   
+   <Text>Welcome to Touch and Go</Text>
   );
 }
 
 const styles = StyleSheet.create({
   largeText: {
-    fontSize: '32px',
+    fontSize: 32,
   },
 });
