4891fe6bb10069c417c90f16a04d1dd557f8c881
Will OKeefe <tidalwithstyle@gmail.com>
2025-02-01 13:03:06 -0500

    modals

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,27 @@
-import { Text, StyleSheet, SafeAreaView, Modal } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, Modal, View, Pressable } from 'react-native';
 import React, { useState } from 'react';
 
 export default function HomeScreen() {
 
   const [clickVis, setClickVis] = useState(false);
-  
+
+  const [swipeVis, setSwipeVis] = useState(false);
+
   return (
     <SafeAreaView style={styles.mainView}>
+      <Modal visible={clickVis} animationType="fade" onRequestClose={() => setClickVis(false)} presentationStyle="pageSheet">
+        <View style={styles.container}>
+          <Text style={styles.largeText}>Click Away!</Text>
+        </View>
+      </Modal>
+      <Modal visible={swipeVis} animationType="fade" onRequestClose={() => setSwipeVis(false)} presentationStyle="pageSheet">
+        <View style={styles.container}>
+          <Text style={styles.largeText}>Swipe!</Text>
+        </View>
+      </Modal>
         <Text style={styles.largeText}>Welcome to Touch and Go</Text>
+        <Pressable onPress={() => setClickVis(true)} style={styles.buttons}>Click</Pressable>
+        <Pressable onPress={() => setSwipeVis(true)} style={styles.buttons}>Swipe</Pressable>
     </SafeAreaView>
   );
 }
@@ -28,5 +42,25 @@ const styles = StyleSheet.create({
   },
   mainView: {
     backgroundColor: 'black',
+  },
+  container: {
+    backgroundColor: 'black',
+    flex: 1,
+    padding: 8,
+  },
+  buttons: {
+    backgroundColor: 'white',
+    fontSize: 24,
+    fontFamily: 'sans-serif-thin',
+    textAlign: 'center',
+    margin: 'auto',
+    padding: 10,
+    marginBottom: 10,
+    marginTop: 10,
+    width: 200,
+    shadowColor: 'red',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 1,
+    shadowRadius: 3,
   },
 });
