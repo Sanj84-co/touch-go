73b4a7ec7123e369f4c9046b0173ff3a207c1eb6
Will OKeefe <tidalwithstyle@gmail.com>
2025-02-01 12:37:23 -0500

    usestate

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,10 @@
-import { Text, StyleSheet, SafeAreaView } from 'react-native';
-import React from 'react';
+import { Text, StyleSheet, SafeAreaView, Modal } from 'react-native';
+import React, { useState } from 'react';
 
 export default function HomeScreen() {
+
+  const [clickVis, setClickVis] = useState(false);
+  
   return (
     <SafeAreaView style={styles.mainView}>
         <Text style={styles.largeText}>Welcome to Touch and Go</Text>
@@ -15,6 +18,13 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     fontFamily: 'sans-serif-thin',
+    borderRadius: 15,
+    padding: 8,
+    shadowColor: 'red',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 1,
+    shadowRadius: 3,
+    margin: 'auto',
   },
   mainView: {
     backgroundColor: 'black',
