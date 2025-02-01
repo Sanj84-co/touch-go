68b67f84dc154532d5305df2785c358f304f1500
Will OKeefe <tidalwithstyle@gmail.com>
2025-02-01 12:11:00 -0500

    styles 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, SafeAreaView } from 'react-native';
 
 export default function HomeScreen() {
   return (
-     <Text style={styles.largeText}>Welcome to Touch and Go</Text>
+    <SafeAreaView style={styles.mainView}>
+        <Text style={styles.largeText}>Welcome to Touch and Go</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   largeText: {
     fontSize: 32,
+  },
+  mainView: {
+    backgroundColor: '#000080',
   },
 });
